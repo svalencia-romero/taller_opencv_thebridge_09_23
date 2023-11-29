9bc14c5bc3dcbeefbb1ae1af09c8ca59f900e7d7
Santiago Valencia <65482310+Kuja182@users.noreply.github.com>
2023-11-29 13:48:46 +0100

    cambios scripts

--- src/face_img_detect.py
+++ src/face_img_detect.py
@@ -8,5 +8,21 @@ face_cascade = cv2.CascadeClassifier(os.path.join(dir_path, "..", "xml", "haarca
 # Read the input image
 img = cv2.imread(os.path.join(dir_path, "..", "img", "faces", "180006.jpg"))
 
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Con esto conseguimos difuminar la imagen y asi detectar mejor las imagenes
+img_filtered = cv2.blur(img, (3, 3))
+
+# Detect faces
+faces = face_cascade.detectMultiScale(img_filtered, 1.1, 4)
+
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+# Display the output
+
+cv2.imshow('img', img)
+cv2.waitKey()
 
