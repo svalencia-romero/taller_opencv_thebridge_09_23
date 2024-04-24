e3a4c6a7cd15bcdc0d41d296abb4738fe1a693f3
Santiago Valencia <65482310+Kuja182@users.noreply.github.com>
2024-04-24 02:47:09 +0200

    cambio ppt, cambios en py y ipynb

--- src/face_img_detect.py
+++ src/face_img_detect.py
@@ -5,19 +5,19 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 face_cascade = cv2.CascadeClassifier(os.path.join(dir_path, "..", "xml", "haarcascade_frontalface_default.xml"))
 
-# Read the input image
+
 img = cv2.imread(os.path.join(dir_path, "..", "img", "faces", "180006.jpg"))
 
-# Convert into grayscale
+# Convertimos a escala de grises
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Con esto conseguimos difuminar la imagen y asi detectar mejor las imagenes
 img_filtered = cv2.blur(img, (3, 3))
 
-# Detect faces
+# Detectamos las caras
 faces = face_cascade.detectMultiScale(img_filtered, 1.1, 4)
 
-# Draw rectangle around the faces
+# Dibujamos rectangulo en cada cara
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
